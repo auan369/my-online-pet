b9e12da9258471b0894e6e3d8a160a34d0d57e8e
Kum yew <lokekumyew@gmail.com>
2024-10-20 15:55:47 +0700

    Made animations toggle

--- src/Pet.js
+++ src/Pet.js
@@ -2,20 +2,32 @@ import React, { useState, useEffect } from 'react';
 import pico from './images/pixil-layer-Layer 1.png';
 import picoSad from './images/pixil-layer-Sad.png';
 import picoHungry from './images/pixil-layer-hungry.png';
+import picoHappy from './images/pixil-layer-Layer 2.png';
 
 function Pet() {
   const [hunger, setHunger] = useState(50);
   const [happiness, setHappiness] = useState(50);
+  // const [emotion, setEmotion] = useState(pico);
+  const [frameIndex, setFrameIndex] = useState(0);
+
+  const getEmotion = () => {
+    if (hunger >= 100) {console.log("hungry", hunger); return picoHungry;};
+    if (happiness <= 0)  {console.log("sad", happiness); return picoSad;}
+    return picoHappy;
+  };
 
   // Game loop - decrease happiness and increase hunger over time
   useEffect(() => {
     const interval = setInterval(() => {
       setHunger(hunger => Math.min(100, hunger + 1));  // Hunger increases
       setHappiness(happiness => Math.max(0, happiness - 1)); // Happiness decreases
-    }, 500);
+      setFrameIndex((prevIndex) => (prevIndex + 1) % 2);
+    }, 300);
 
     return () => clearInterval(interval); // Cleanup interval on unmount
   }, []);
+
+  
 
   // Handle feeding the Pet
   const feedPet = () => {
@@ -28,16 +40,12 @@ function Pet() {
   };
 
   // Render the pet's emotion based on hunger and happiness
-  const getEmotion = () => {
-    if (hunger >= 100) return picoHungry;
-    if (happiness <= 0) return picoSad;
-    return pico;
-  };
+
 
   return (
     <div>
       {/* <div className="pet">{getEmotion()}</div> */}
-      <img src={getEmotion()} className = "pico" alt="logo" />
+      <img src={frameIndex? pico: getEmotion()} className = "pico" alt="logo" />
       <div className="stats">
         Hunger: {hunger}/100 <br />
         Happiness: {happiness}/100
